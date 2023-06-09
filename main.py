7fb33f1adb648544d77fadd6aae0b9a2e1f1fdaf
Miedziu <krzysztofmiedziak2000@gmail.com>
2023-06-09 18:35:14 +0200

    added loading yml function

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('hello world')
+import yaml
+
+def load_yaml_file(filepath):
+    try:
+        with open(filepath, 'r') as yaml_file:
+            data = yaml.safe_load(yaml_file)
+        print("Pomyślnie wczytano plik YAML.")
+        return data
+    except yaml.YAMLError as e:
+        print(f"Błąd: plik YAML jest niepoprawny: {e}")
+        return None
+    except Exception as e:
+        print(f"Błąd: {e}")
+        return None
