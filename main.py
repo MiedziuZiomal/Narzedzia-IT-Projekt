2208386b6ee04056eb162225827613b3dae656f5
Miedziu <krzysztofmiedziak2000@gmail.com>
2023-06-09 17:59:55 +0200

    created function to read and verify JSON

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('hello world')
+import json
+
+def load_json_file(filepath):
+    try:
+        with open(filepath, 'r') as json_file:
+            data = json.load(json_file)
+        print("Pomyślnie wczytano plik JSON.")
+        return data
+    except json.JSONDecodeError:
+        print("Błąd: plik JSON jest niepoprawny.")
+        return None
+    except Exception as e:
+        print(f"Błąd: {e}")
+        return None
