c1b805c7f36204f6f4081066d374a062a92fddf9
Miedziu <krzysztofmiedziak2000@gmail.com>
2023-06-09 18:37:39 +0200

    created writing to YML function

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('hello world')
+import yaml
+
+def write_to_yaml_file(filepath, data):
+    try:
+        with open(filepath, 'w') as yaml_file:
+            yaml.dump(data, yaml_file)
+        print(f"Pomyślnie zapisano dane do pliku YAML: {filepath}")
+    except Exception as e:
+        print(f"Błąd podczas zapisywania do pliku YAML: {e}")
