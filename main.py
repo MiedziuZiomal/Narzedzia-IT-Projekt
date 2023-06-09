c1b1274401754b585810183850c89039c3958014
Miedziu <krzysztofmiedziak2000@gmail.com>
2023-06-09 20:09:47 +0200

    write)to_xml_function

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('hello world')
+def write_to_xml_file(filepath, data):
+    try:
+        tree = ET.ElementTree(data)
+        with open(filepath, 'wb') as xml_file:
+            tree.write(xml_file)
+        print(f"Pomyślnie zapisano dane do pliku XML: {filepath}")
+    except Exception as e:
+        print(f"Błąd podczas zapisywania do pliku XML: {e}")
