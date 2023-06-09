5d43470eeb8cb8157281a7beaead6764d1bbffe9
Miedziu <krzysztofmiedziak2000@gmail.com>
2023-06-09 20:05:40 +0200

    added xml handling function

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('hello world')
+import xml.etree.ElementTree as ET
+
+def load_xml_file(filepath):
+    try:
+        tree = ET.parse(filepath)
+        root = tree.getroot()
+        print("Pomyślnie wczytano plik XML.")
+        return root
+    except ET.ParseError:
+        print("Błąd: plik XML jest niepoprawny.")
+        return None
+    except Exception as e:
+        print(f"Błąd: {e}")
+        return None
